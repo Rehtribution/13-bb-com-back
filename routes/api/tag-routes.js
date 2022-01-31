85deab5a24ee68f8d5a7dd1fec1f1e9eea0d943c
Reht Cavazos <reht.dev@gmail.com>
2022-01-31 14:20:15 -0600

    tag-routes get all and find one successful.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,18 +3,35 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
+// find all tags
+// be sure to include its associated Product data
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    include: {
+      model: Product,
+      through: ProductTag,
+    }
+  }).then(item => res.json(item))
 });
 
+// find a single tag by its `id`
+// be sure to include its associated Product data
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
-});
+    Tag.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: Product,
+          through: ProductTag,
+        }
+      ]
+    }).then(item => res.json(item))
+  });
 
-router.post('/', (req, res) => {
   // create a new tag
+router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
